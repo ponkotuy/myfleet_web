943345b829107549ba0f3c863d81c6af94d80e08
astel <kiyomillefeuille@gmail.com>
2015-11-09 19:45:44 +0900

    fix java version and fix login URL

--- app/views/URL.scala
+++ app/views/URL.scala
@@ -8,7 +8,7 @@ package views
 object URL {
   val Host = "https://myfleet.moe"
   def Index = Host
-  val Entire = s"${Host}/entire"
+  val Entire = s"${Host}/passwd/entire"
   val Statistics = s"${Entire}/activities"
   val Login = s"${Entire}/login"
   val Favicon = s"${Host}/favicon.ico"
