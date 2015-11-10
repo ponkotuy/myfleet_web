94d66a4a361964b1d983818416c4b71b9c4fe96b
astel <kiyomillefeuille@gmail.com>
2015-11-10 22:27:52 +0900

    消し忘れ

--- app/views/URL.scala
+++ app/views/URL.scala
@@ -9,7 +9,6 @@ object URL {
   val Host = "https://myfleet.moe"
   def Index = Host
   val Entire = s"${Host}/entire"
-  val Entire = s"${Host}/passwd/entire"
   val Statistics = s"${Entire}/activities"
   val Login = s"${Entire}/passwd/login"
   val Favicon = s"${Host}/favicon.ico"
