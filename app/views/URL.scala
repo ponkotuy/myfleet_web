49664dd29ee554f4ea82e95965b1802ef887ab94
astel <kiyomillefeuille@gmail.com>
2015-11-10 22:26:40 +0900

    そもそも修正する場所を間違っていた

--- app/views/URL.scala
+++ app/views/URL.scala
@@ -8,9 +8,10 @@ package views
 object URL {
   val Host = "https://myfleet.moe"
   def Index = Host
+  val Entire = s"${Host}/entire"
   val Entire = s"${Host}/passwd/entire"
   val Statistics = s"${Entire}/activities"
-  val Login = s"${Entire}/login"
+  val Login = s"${Entire}/passwd/login"
   val Favicon = s"${Host}/favicon.ico"
   val Assets = s"${Host}/assets"
 }
